81df8192e2e31021e5feffc11c30d595adb110bb
Jochen Ott <jott@cern.ch>
2014-11-18 11:06:51 +0100

    Uncomment couts in test genjets module

--- NtupleWriter/plugins/NtupleWriterCMSSWTests.cc
+++ NtupleWriter/plugins/NtupleWriterCMSSWTests.cc
@@ -12,13 +12,13 @@ class NtupleWriterTestGenJets : public edm::EDAnalyzer {
           edm::Handle<reco::GenJetCollection> h_genjets;
           event.getByLabel("slimmedGenJets", h_genjets);
           for(const auto & gj : *h_genjets){
-              //std::cout << "Genjet p4 = " << gj.p4() << std::endl;
+              std::cout << "Genjet p4 = " << gj.p4() << std::endl;
               reco::Candidate::LorentzVector p4_constituent_sum;
               for(const auto & constituent : gj){
-                  //p4_constituent_sum += constituent.p4();
-                  //std::cout << "   constituent: " << constituent.p4() << std::endl;
+                  p4_constituent_sum += constituent.p4();
+                  std::cout << "   constituent: " << constituent.p4() << std::endl;
               }
-              //std::cout << " constituent sum p4 = " << p4_constituent_sum << std::endl;
+              std::cout << " constituent sum p4 = " << p4_constituent_sum << std::endl;
           }
       }
 };
