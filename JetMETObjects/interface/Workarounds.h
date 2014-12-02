4da16e82433fbc2ace64f8ce3e2f4f64680547d6
Jochen Ott <jott@cern.ch>
2014-12-02 10:17:23 +0100

    add JetMETObjects for jet energy corrections

---
+++ JetMETObjects/interface/Workarounds.h
@@ -0,0 +1,28 @@
+#include <sstream>
+#include <stdexcept>
+
+// workarounds to make standalone compilation easier. -- Jochen Ott, Nov. 2014
+
+using namespace std;
+
+namespace edm {
+    class LogError{
+    public:
+        explicit LogError(const std::string & s){
+            msg << s << " ";
+        }
+        
+        template<typename T>
+        LogError & operator<<(T && t){
+            msg << std::forward<T>(t);
+            return *this;
+        }
+        
+        ~LogError(){
+            throw std::runtime_error(msg.str());
+        }
+        
+    private:
+        std::stringstream msg;
+    };
+}
