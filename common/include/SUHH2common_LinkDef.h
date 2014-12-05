6a06cf01b7f68585156d67d954d62eff7f3452f2
Thomas <thomas.peiffer@desy.de>
2014-12-05 14:42:05 +0100

    ReconstructionHypothesis container added

---
+++ common/include/SUHH2common_LinkDef.h
@@ -0,0 +1,13 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ nestedclass;
+
+#pragma link C++ class ReconstructionHypothesis+;
+#pragma link C++ class std::vector<ReconstructionHypothesis>+;
+#pragma link C++ struct qualityflag+;
+
+#endif // __CINT__
