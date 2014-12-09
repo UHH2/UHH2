a9c0d12414ecee85c3a42e95d1244c95e0c46d65
Jochen Ott <jott@cern.ch>
2014-12-09 10:17:45 +0100

    Improve test of jet energy correction module

--- common/test/TestJetCorrections.cpp
+++ common/test/TestJetCorrections.cpp
@@ -3,26 +3,54 @@
 #include "UHH2/common/include/JetCorrections.h"
 
 using namespace uhh2;
+using namespace std;
 
 class TestJetCorrections: public uhh2::AnalysisModule {
 public:
-    explicit TestJetCorrections(Context & ctx){
-        modules.emplace_back(new JetPrinter("before correction", 10.0));
-        modules.emplace_back(new JetCorrector(JERFiles::PHYS14_L123_MC));
-        modules.emplace_back(new JetPrinter("after correction", 10.0));
+    explicit TestJetCorrections(Context & ctx) {
+        jec_mc.reset(new JetCorrector(JERFiles::PHYS14_L123_MC));
+        jec_data.reset(new JetCorrector(JERFiles::PHYS14_L123_DATA));
     }
     
     virtual bool process(Event & e) override;
     
 private:
-    std::vector<std::unique_ptr<AnalysisModule>> modules;
+    std::unique_ptr<AnalysisModule> jec_mc, jec_data;
 };
+
+namespace {
+
+// return value: 0 = exactly the same pt, 1 = almost the same pt (rel. pt diff < 1e-3), 2 = not the same pt, 3 = jet vectors do not have the same size
+int compare_jets(const vector<Jet> & jets1, const vector<Jet> & jets2){
+    if(jets1.size()!= jets2.size()) return 3;
+    const size_t njets = jets1.size();
+    float max_reldiff = 0.0f;
+    for(size_t i=0; i<njets; ++i){
+        float reldiff = fabs(jets1[i].pt() - jets2[i].pt()) / jets1[i].pt();
+        max_reldiff = std::max(reldiff, max_reldiff);
+    }
+    if(max_reldiff == 0.0f) return 0;
+    else if(max_reldiff < 1e-3f) return 1;
+    else return 2;
+}
+
+    
+}
 
 
 bool TestJetCorrections::process(Event & e){
-    for(auto & m : modules){
-        m->process(e);
-    }
+    if(e.jets->empty()) return true;
+    // NOTE: we test here only that the jet correction module does 'something' to the jets which looks reasonable;
+    // the size of the corrections are not tested, as this is a matter to verify centrally in CMS
+    auto jets_before = *e.jets;
+    jec_mc->process(e);
+    // re-applying MC jet energy corrections should not change anything:
+    auto jets_after_mc_jec = *e.jets;
+    assert(compare_jets(jets_before, jets_after_mc_jec) <= 1);
+    jec_data->process(e);
+    auto jets_after_data_jec = *e.jets;
+    // applying data jet energy corrections on MC should change the pt considerably, but not the number of jets:
+    assert(compare_jets(jets_before, jets_after_data_jec) == 2);
     return true;
 }
 
