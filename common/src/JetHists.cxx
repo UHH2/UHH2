afa51216a1a47b4c302b07145ae5f82b81871d18
Heiner Tholen <heiner.tholen@cern.ch>
2015-01-21 17:25:19 +0100

    larger eta bounds and csv discriminator

--- common/src/JetHists.cxx
+++ common/src/JetHists.cxx
@@ -17,26 +17,31 @@ JetHists::JetHists(Context & ctx,
     number = book<TH1F>("number","number of jets",21, -.5, 20.5);
     
     pt = book<TH1F>("pt","p_{T} all jets",50,20,1500);
-    eta = book<TH1F>("eta","#eta all jets",50,-4,4);
+    eta = book<TH1F>("eta","#eta all jets",100,-5,5);
     phi = book<TH1F>("phi","#phi all jets",50,-M_PI,M_PI);
-    
+    csv = book<TH1F>("csv","csv-disriminator all jets",50,0,1);
+
     pt_1 = book<TH1F>("pt_1","p_{T} jet 1",50,100,1500);
-    eta_1 = book<TH1F>("eta_1","#eta jet 1",50,-4,4);
+    eta_1 = book<TH1F>("eta_1","#eta jet 1",100,-5,5);
     phi_1 = book<TH1F>("phi_1","#phi jet 1",50,-M_PI,M_PI);
+    csv_1 = book<TH1F>("csv_1","csv-disriminator jet 1",50,0,1);
     deltaRmin_1 = book<TH1F>("deltaRmin_1", "#Delta R_{min}(first jet,nearest jet)", 40, 0, 2.0);
     
     pt_2 = book<TH1F>("pt_2","p_{T} jet 2",50,20,1000);
-    eta_2 = book<TH1F>("eta_2","#eta jet 2",50,-4,4);
+    eta_2 = book<TH1F>("eta_2","#eta jet 2",100,-5,5);
     phi_2 = book<TH1F>("phi_2","#phi jet 2",50,-M_PI,M_PI);
+    csv_2 = book<TH1F>("csv_2","csv-disriminator jet 2",50,0,1);
     deltaRmin_2 = book<TH1F>("deltaRmin_2", "#Delta R_{min}(2nd jet,nearest jet)", 40, 0, 2.0);
 
     pt_3 = book<TH1F>("pt_3","p_{T} jet 3",50,20,500);
-    eta_3 = book<TH1F>("eta_3","#eta jet 3",50,-4,4);
+    eta_3 = book<TH1F>("eta_3","#eta jet 3",100,-5,5);
     phi_3 = book<TH1F>("phi_3","#phi jet 3",50,-M_PI,M_PI);
+    csv_3 = book<TH1F>("csv_3","csv-disriminator jet 3",50,0,1);
 
     pt_4 = book<TH1F>("pt_4","p_{T} jet 4",50,20,250);
-    eta_4 = book<TH1F>("eta_4","#eta jet 4",50,-4,4);
+    eta_4 = book<TH1F>("eta_4","#eta jet 4",100,-5,5);
     phi_4 = book<TH1F>("phi_4","#phi jet 4",50,-M_PI,M_PI);
+    csv_4 = book<TH1F>("csv_4","csv-disriminator jet 4",50,0,1);
 
     m_1 = book<TH1F>("m_1","M^{jet 1} [GeV/c^{2}]", 100, 0, 300);
     m_2 = book<TH1F>("m_2","M^{jet 2} [GeV/c^{2}]", 100, 0, 300);
@@ -59,6 +64,7 @@ void JetHists::fill(const Event & event){
         pt->Fill(jet.pt(), w);
         eta->Fill(jet.eta(), w);
         phi->Fill(jet.phi(), w);
+        csv->Fill(jet.btag_combinedSecondaryVertex(), w);
     }
     
     if(jets->size() > 0){
@@ -66,6 +72,7 @@ void JetHists::fill(const Event & event){
         pt_1->Fill(jet.pt(), w);
         eta_1->Fill(jet.eta(), w);
         phi_1->Fill(jet.phi(), w);
+        csv_1->Fill(jet.btag_combinedSecondaryVertex(), w);
         m_1->Fill(jet.v4().M(), w);
         auto next_jet = nextJet(jet, *jets);
         auto drmin = next_jet ? deltaR(jet, *next_jet) : numeric_limits<float>::infinity();
@@ -76,6 +83,7 @@ void JetHists::fill(const Event & event){
         pt_2->Fill(jet.pt(), w);
         eta_2->Fill(jet.eta(), w);
         phi_2->Fill(jet.phi(), w);
+        csv_2->Fill(jet.btag_combinedSecondaryVertex(), w);
         m_2->Fill(jet.v4().M(), w);
         auto next_jet = nextJet(jet, *jets);
         auto drmin = next_jet ? deltaR(jet, *next_jet) : numeric_limits<float>::infinity();
@@ -87,6 +95,7 @@ void JetHists::fill(const Event & event){
         pt_3->Fill(jet.pt(), w);
         eta_3->Fill(jet.eta(), w);
         phi_3->Fill(jet.phi(), w);
+        csv_3->Fill(jet.btag_combinedSecondaryVertex(), w);
         m_3->Fill(jet.v4().M(), w);
     }
     if(jets->size() > 3){
@@ -94,6 +103,7 @@ void JetHists::fill(const Event & event){
         pt_4->Fill(jet.pt(), w);
         eta_4->Fill(jet.eta(), w);
         phi_4->Fill(jet.phi(), w);
+        csv_4->Fill(jet.btag_combinedSecondaryVertex(), w);
         m_4->Fill(jet.v4().M(), w);
     }
     
