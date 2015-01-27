8956b88a22565c2e416a2dd4610906bf756bb5e0
Jochen Ott <jott@cern.ch>
2015-01-27 17:21:32 +0100

    Add more tests for genparticle pruning in single-T' sample

--- core/plugins/FilterStrangeGenParticles.cxx
+++ core/plugins/FilterStrangeGenParticles.cxx
@@ -9,7 +9,7 @@ class FilterStrangeGenParticles: public edm::EDFilter {
    public:
       explicit FilterStrangeGenParticles(const edm::ParameterSet& cfg) {
           src = cfg.getParameter<edm::InputTag>("src");
-          n_top_expected = cfg.getParameter<int>("n_top_expected");
+          n_top_expected = cfg.getUntrackedParameter<int>("n_top_expected", -1);
       }
 
    private:
@@ -34,31 +34,72 @@ bool FilterStrangeGenParticles::filter(edm::Event & event, const edm::EventSetup
                 cout << "T' has " << gp.numberOfDaughters() << " daughters." << endl;
                 return true;
             }
+            auto tprime_d0 = gp.daughter(0);
+            auto tprime_d1 = gp.daughter(1);
+            
+            int pdgid_lower = abs(tprime_d0->pdgId());
+            int pdgid_higher = abs(tprime_d1->pdgId());
+            if(pdgid_lower > pdgid_higher){
+                swap(pdgid_lower, pdgid_higher);
+                swap(tprime_d0, tprime_d1);
+            }
+            if(pdgid_lower == 6 && pdgid_higher == 25){// OK: tH
+                continue;
+            }
+            //TODO: check all known decay channels!
+            else{
+                cout << "T' does not decay to top + Higgs" << endl;
+                return true;
+            }
         }
         if(abs(gp.pdgId()) == 6){
             ++n_top;
-            if(gp.numberOfDaughters() != 2){
+            if(gp.numberOfDaughters() < 2 || gp.numberOfDaughters() > 3){
                 cout << "t has " << gp.numberOfDaughters() << " daughters." << endl;
                 return true;
             }
-            auto topd0 = gp.daughter(0);
-            auto topd1 = gp.daughter(1);
-            int pdgid_lower = abs(topd0->pdgId());
-            int pdgid_higher = abs(topd1->pdgId());
-            if(pdgid_lower > pdgid_higher){
-                swap(pdgid_lower, pdgid_higher);
-                swap(topd0, topd1);
+            if(gp.numberOfDaughters() == 2){
+                auto topd0 = gp.daughter(0);
+                auto topd1 = gp.daughter(1);
+                int pdgid_lower = abs(topd0->pdgId());
+                int pdgid_higher = abs(topd1->pdgId());
+                if(pdgid_lower > pdgid_higher){
+                    swap(pdgid_lower, pdgid_higher);
+                    swap(topd0, topd1);
+                }
+                if(pdgid_lower != 5){
+                    cout << "t does not have b as daughter" << endl;
+                    return true;
+                }
+                if(pdgid_higher != 24){
+                    cout << "t does not have W as daughter" << endl;
+                    return true;
+                }
+                if(topd1->numberOfDaughters() != 2){
+                    cout << "W from top has != 2 daughters" << endl;
+                    return true;
+                }
             }
-            if(pdgid_lower != 5){
-                cout << "t does not have b as daughter" << endl;
-                return true;
+            else{
+                // intermediate W missing?
+                auto topd0 = gp.daughter(0);
+                auto topd1 = gp.daughter(1);
+                auto topd2 = gp.daughter(2);
+                std::set<int> pdgids;
+                pdgids.insert(std::abs(topd0->pdgId()));
+                pdgids.insert(std::abs(topd1->pdgId()));
+                pdgids.insert(std::abs(topd2->pdgId()));
+                
+                if(pdgids.find(5)==pdgids.end()){
+                    cout << "top has 3 daughters, but no b!" << endl;
+                }
+                // TODO: could check more, e.g. that the two others are consistent with coming from a W.
+                //return true;
             }
-            if(pdgid_higher != 24){
-                cout << "t does not have W as daughter" << endl;
-                return true;
-            }
-            if(topd1->numberOfDaughters() != 2){
-                cout << "W from top has != 2 daughters" << endl;
+        }
+        else if(abs(gp.pdgId()) == 25){ // Higgs
+            if(gp.numberOfDaughters() != 2){
+                cout << "Higgs has != 2 daughters!" << endl;
                 return true;
             }
         }
