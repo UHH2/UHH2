7e681fdc2ed2540def52745bd9d2eda01e2478ff
Daniel <daniel.gonzalez@desy.de>
2015-01-27 18:32:04 +0100

    added topjet hist

--- common/include/JetHists.h
+++ common/include/JetHists.h
@@ -3,6 +3,12 @@
 #include "UHH2/core/include/Hists.h"
 #include "UHH2/core/include/Event.h"
 
+#include <vector>
+#include <string>
+
+using namespace uhh2;
+using namespace std;
+
 /** \brief Common jet histograms such as multiplicity, pt, eta, isolation, etc.
  *
  *
@@ -11,21 +17,73 @@
  *   - no log-y version of the histogram; this is a plotting choice.
  *   - if collection is empty, event.jets will be used
  */
-class JetHists: public uhh2::Hists {
-public:
-    JetHists(uhh2::Context & ctx,
-             const std::string & dirname,
-             const std::string & collection = "");
-    
-    virtual void fill(const uhh2::Event & ev) override;
-    
-private:
-    
-    TH1F *number, *pt, *eta, *phi, *csv;
-    TH1F *pt_1, *eta_1, *phi_1, *m_1, *csv_1, *deltaRmin_1;
-    TH1F *pt_2, *eta_2, *phi_2, *m_2, *csv_2, *deltaRmin_2;
-    TH1F *pt_3, *eta_3, *phi_3, *m_3, *csv_3;
-    TH1F *pt_4, *eta_4, *phi_4, *m_4, *csv_4;
 
-    uhh2::Event::Handle<std::vector<Jet> > h_jets;
+struct jetHist{
+  TH1F* pt, *eta, *phi, *mass, *csv; 
+}; 
+
+class JetHistsBase: public uhh2::Hists {
+ public:
+  JetHistsBase(uhh2::Context & ctx,
+	      const std::string & dirname);
+
+
+  jetHist jetHistsBooker(string suffix, double minPt, double maxPt);
+  
+  template <typename T>
+    void jetHistFiller(T jet,jetHist jet_hist, double  weight);
+
+  virtual void fill(const uhh2::Event & ev) =0;
 };
+
+
+
+
+class JetHists: public JetHistsBase {
+public:
+
+  JetHists(uhh2::Context & ctx,
+	   const std::string & dirname,
+	   const std::string & collection = "");
+  
+  virtual void fill(const uhh2::Event & ev) override;
+  
+  
+  
+ private:
+  
+  TH1F *number;
+  TH1F *deltaRmin_1, *deltaRmin_2;
+  
+  jetHist alljets;
+  vector<jetHist> single_jetHists;
+  
+  uhh2::Event::Handle<std::vector<Jet> > h_jets;
+};
+
+
+
+class TopJetHists: public JetHistsBase{
+ public: 
+  TopJetHists(uhh2::Context & ctx,
+	      const std::string & dirname,
+	      const std::string & collection = "");
+  
+ 
+  
+  virtual void fill(const uhh2::Event & ev) override;
+   
+
+ private:
+  
+  TH1F *number;
+  TH1F *deltaRmin_1, *deltaRmin_2;
+  
+  jetHist alljets;
+  vector<jetHist> single_jetHists;
+  
+  uhh2::Event::Handle<std::vector<TopJet> > h_topjets;
+
+};
+
+
--- common/src/JetHists.cxx
+++ common/src/JetHists.cxx
@@ -11,42 +11,56 @@
 using namespace uhh2;
 using namespace std;
 
+
+JetHistsBase::JetHistsBase(uhh2::Context & ctx,
+			   const std::string & dirname): Hists(ctx, dirname){}
+
+jetHist JetHistsBase::jetHistsBooker(string suffix, double minPt, double maxPt){
+  jetHist jet_hist;
+  
+  string histname;
+  if(suffix.compare("")==0)
+    histname = suffix;
+  else
+    histname = string("_")+suffix;
+
+  jet_hist.pt = book<TH1F>((string("pt")+histname).c_str(),(string("p_{T} jet ")+suffix).c_str(),50,minPt,maxPt);
+  jet_hist.eta = book<TH1F>((string("eta")+histname).c_str(),(string("#eta jet ")+suffix).c_str(),100,-5,5);
+  jet_hist.phi = book<TH1F>((string("phi")+histname).c_str(),(string("#phi jet ")+suffix).c_str(),50,-M_PI,M_PI);
+  jet_hist.mass = book<TH1F>((string("mass")+histname).c_str(),(string("M^{jet ")+suffix+string("} [GeV/c^{2}]")).c_str(), 100, 0, 300);
+  jet_hist.csv = book<TH1F>((string("csv")+histname).c_str(),(string("csv-disriminator jet ")+suffix).c_str(),50,0,1);
+
+  return jet_hist;
+}
+
+template <typename T>
+void JetHistsBase::jetHistFiller(T jet, jetHist jet_hist, double  weight){
+  jet_hist.pt->Fill(jet.pt(), weight);
+  jet_hist.eta->Fill(jet.eta(), weight);
+  jet_hist.phi->Fill(jet.phi(), weight);
+  jet_hist.mass->Fill(jet.v4().M(), weight);
+  jet_hist.csv->Fill(jet.btag_combinedSecondaryVertex(), weight);
+}
+
 JetHists::JetHists(Context & ctx,
                    const std::string & dname,
-                   const std::string & collection): Hists(ctx, dname){
+                   const std::string & collection): JetHistsBase(ctx, dname){
+
+
     number = book<TH1F>("number","number of jets",21, -.5, 20.5);
     
-    pt = book<TH1F>("pt","p_{T} all jets",50,20,1500);
-    eta = book<TH1F>("eta","#eta all jets",100,-5,5);
-    phi = book<TH1F>("phi","#phi all jets",50,-M_PI,M_PI);
-    csv = book<TH1F>("csv","csv-disriminator all jets",50,0,1);
+    alljets = jetHistsBooker("",20,1500);
 
-    pt_1 = book<TH1F>("pt_1","p_{T} jet 1",50,100,1500);
-    eta_1 = book<TH1F>("eta_1","#eta jet 1",100,-5,5);
-    phi_1 = book<TH1F>("phi_1","#phi jet 1",50,-M_PI,M_PI);
-    csv_1 = book<TH1F>("csv_1","csv-disriminator jet 1",50,0,1);
+    vector<double> minPt {20,20,20,20};
+    vector<double> maxPt {1500,1000,500,350};
+    vector<string> h_suffix {"1","2","3","4"};
+
+    for(unsigned int i =0; i<4; i++){
+      single_jetHists.push_back(jetHistsBooker(h_suffix[i],minPt[i],maxPt[i]));
+    }
+  
     deltaRmin_1 = book<TH1F>("deltaRmin_1", "#Delta R_{min}(first jet,nearest jet)", 40, 0, 2.0);
-    
-    pt_2 = book<TH1F>("pt_2","p_{T} jet 2",50,20,1000);
-    eta_2 = book<TH1F>("eta_2","#eta jet 2",100,-5,5);
-    phi_2 = book<TH1F>("phi_2","#phi jet 2",50,-M_PI,M_PI);
-    csv_2 = book<TH1F>("csv_2","csv-disriminator jet 2",50,0,1);
     deltaRmin_2 = book<TH1F>("deltaRmin_2", "#Delta R_{min}(2nd jet,nearest jet)", 40, 0, 2.0);
-
-    pt_3 = book<TH1F>("pt_3","p_{T} jet 3",50,20,500);
-    eta_3 = book<TH1F>("eta_3","#eta jet 3",100,-5,5);
-    phi_3 = book<TH1F>("phi_3","#phi jet 3",50,-M_PI,M_PI);
-    csv_3 = book<TH1F>("csv_3","csv-disriminator jet 3",50,0,1);
-
-    pt_4 = book<TH1F>("pt_4","p_{T} jet 4",50,20,250);
-    eta_4 = book<TH1F>("eta_4","#eta jet 4",100,-5,5);
-    phi_4 = book<TH1F>("phi_4","#phi jet 4",50,-M_PI,M_PI);
-    csv_4 = book<TH1F>("csv_4","csv-disriminator jet 4",50,0,1);
-
-    m_1 = book<TH1F>("m_1","M^{jet 1} [GeV/c^{2}]", 100, 0, 300);
-    m_2 = book<TH1F>("m_2","M^{jet 2} [GeV/c^{2}]", 100, 0, 300);
-    m_3 = book<TH1F>("m_3","M^{jet 3} [GeV/c^{2}]", 100, 0, 300);
-    m_4 = book<TH1F>("m_4","M^{jet 4} [GeV/c^{2}]", 100, 0, 300);
 
     h_jets = ctx.get_handle<std::vector<Jet> >(collection);
 }
@@ -60,51 +74,94 @@ void JetHists::fill(const Event & event){
     assert(jets);
     number->Fill(jets->size(), w);
     
-    for(const auto & jet : *jets){
-        pt->Fill(jet.pt(), w);
-        eta->Fill(jet.eta(), w);
-        phi->Fill(jet.phi(), w);
-        csv->Fill(jet.btag_combinedSecondaryVertex(), w);
+
+    for(unsigned int i = 0; i <jets->size(); i++){
+      const auto & jet = (*jets)[i];
+
+      jetHistFiller(jet,alljets,w);
+      
+      if(i<4){
+	jetHistFiller(jet,single_jetHists.at(i),w);
+	
+	if(i<2){
+	  
+	  auto next_jet = closestParticle(jet, *jets);
+	  auto drmin = next_jet ? deltaR(jet, *next_jet) : numeric_limits<float>::infinity();
+	  
+	  //cout<<drmin<<endl;
+
+	  if(i==0)deltaRmin_1->Fill(drmin, w);
+	  else deltaRmin_2->Fill(drmin, w);
+	}
+      }
     }
-    
-    if(jets->size() > 0){
-        const auto & jet = (*jets)[0];
-        pt_1->Fill(jet.pt(), w);
-        eta_1->Fill(jet.eta(), w);
-        phi_1->Fill(jet.phi(), w);
-        csv_1->Fill(jet.btag_combinedSecondaryVertex(), w);
-        m_1->Fill(jet.v4().M(), w);
-        auto next_jet = nextJet(jet, *jets);
-        auto drmin = next_jet ? deltaR(jet, *next_jet) : numeric_limits<float>::infinity();
-        deltaRmin_1->Fill(drmin, w);
-    }
-    if(jets->size() > 1){
-        const auto & jet = (*jets)[1];
-        pt_2->Fill(jet.pt(), w);
-        eta_2->Fill(jet.eta(), w);
-        phi_2->Fill(jet.phi(), w);
-        csv_2->Fill(jet.btag_combinedSecondaryVertex(), w);
-        m_2->Fill(jet.v4().M(), w);
-        auto next_jet = nextJet(jet, *jets);
-        auto drmin = next_jet ? deltaR(jet, *next_jet) : numeric_limits<float>::infinity();
-        deltaRmin_2->Fill(drmin, w);
-    }
-    
-    if(jets->size() > 2){
-        const auto & jet = (*jets)[2];
-        pt_3->Fill(jet.pt(), w);
-        eta_3->Fill(jet.eta(), w);
-        phi_3->Fill(jet.phi(), w);
-        csv_3->Fill(jet.btag_combinedSecondaryVertex(), w);
-        m_3->Fill(jet.v4().M(), w);
-    }
-    if(jets->size() > 3){
-        const auto & jet = (*jets)[3];
-        pt_4->Fill(jet.pt(), w);
-        eta_4->Fill(jet.eta(), w);
-        phi_4->Fill(jet.phi(), w);
-        csv_4->Fill(jet.btag_combinedSecondaryVertex(), w);
-        m_4->Fill(jet.v4().M(), w);
-    }
-    
 }
+
+//----------------------------------------------
+//----------------------------------------------
+//----------------------------------------------
+//----------------------------------------------
+/** TopJet Histograms
+ *
+ *
+ *
+ *
+ */
+
+TopJetHists::TopJetHists(Context & ctx,
+			 const std::string & dname,
+			 const std::string & collection): JetHistsBase(ctx, dname){
+
+  number = book<TH1F>("number","number of jets",21, -.5, 20.5);
+  
+  alljets = jetHistsBooker("",20,1500);
+ 
+  vector<double> minPt {20,20,20,20};
+  vector<double> maxPt {1500,1000,500,350};
+  vector<string> h_suffix {"1","2","3","4"};
+  
+  for(unsigned int i =0; i<4; i++)
+    single_jetHists.push_back(jetHistsBooker(h_suffix[i],minPt[i],maxPt[i]));
+    
+  deltaRmin_1 = book<TH1F>("deltaRmin_1", "#Delta R_{min}(first jet,nearest jet)", 40, 0, 2.0);
+  deltaRmin_2 = book<TH1F>("deltaRmin_2", "#Delta R_{min}(2nd jet,nearest jet)", 40, 0, 2.0);
+
+  h_topjets = ctx.get_handle<std::vector<TopJet> >(collection);
+  
+}
+
+void TopJetHists::fill(const Event & event){
+    auto w = event.weight;
+
+    const auto jets = (event.get_state(h_topjets) == GenericEvent::state::valid)
+                      ? &event.get(h_topjets) : event.topjets;
+
+    //const auto jets =  event.topjets;
+
+    assert(jets);
+    number->Fill(jets->size(), w);
+    
+
+    for(unsigned int i = 0; i <jets->size(); i++){
+      const auto & jet = (*jets)[i];
+
+      jetHistFiller(jet,alljets,w);
+      
+      if(i<4){
+	jetHistFiller(jet,single_jetHists.at(i),w);  
+
+	if(i<2){
+	  
+	  auto next_jet = closestParticle(jet, *jets);
+	  auto drmin = next_jet ? deltaR(jet, *next_jet) : numeric_limits<float>::infinity();
+	  
+	  //cout<<drmin<<endl;
+
+	  if(i==0)deltaRmin_1->Fill(drmin, w);
+	  else deltaRmin_2->Fill(drmin, w);
+	}
+
+      }
+    }
+}
+
