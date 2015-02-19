6686327ba141433ea1ce484a6b34bd1874b743d9
Jochen Ott <jott@cern.ch>
2015-02-19 16:13:54 +0100

    Simplify metadata handling
    The new metadata handling can only assign metadata to the whole sample,
    and not (as in the old mechanism) to ranges of events. On the plus-side,
    the code and interface is a lot simpler and is integrated into the
    methods accessing configuration values via Context::get.

--- core/test/testall.sh
+++ core/test/testall.sh
@@ -9,14 +9,9 @@ exec_checked bin/create_testtree
 exec_checked sframe_main test/Test-a.xml
 
 # metadata tests:
-
-exec_checked sframe_main test/Test-MetaDataA.xml
-exec_checked sframe_main test/Test-MetaDataB.xml
-exec_checked sframe_main test/Test-MetaDataC.xml
-
-# the metadata value read in test C should round-trip to the output of test C, so
-# re-running test C on its output should succeed:
-exec_checked sframe_main test/Test-MetaDataC-RoundTrip.xml
+exec_checked sframe_main test/Test-MetadataOut.xml
+exec_checked sframe_main test/Test-MetadataIn.xml
+exec_checked sframe_main test/Test-MetadataIn2.xml
 
 exec_checked bin/test
 
