85c21fd2811560c9a5f3ba7502c135561675e038
Jochen Ott <jott@cern.ch>
2015-03-02 17:45:20 +0100

    Add perfect argument forwarding for AndSelection
    In-place construction via the AndSelection::add now
    has perfect argument forwarding.

--- core/include/Selection.h
+++ core/include/Selection.h
@@ -40,10 +40,10 @@ public:
     
     /// add the selection module. The description is used for the cutflow histogram as x-axis label and in the summary printed at the end of the SFrame job.
     template<typename TSEL, typename... CARGS>
-    void add(const std::string & description, CARGS... cargs){
+    void add(const std::string & description, CARGS &&... cargs){
         // note: writing the add method this way instead of taking a pointer to Selection directly circumvents
         // the issues with memory ownership nicely.
-        selections.emplace_back(new TSEL(cargs...));
+        selections.emplace_back(new TSEL(std::forward<CARGS>(cargs)...));
         descriptions.push_back(description);
     }
 
