fb7413dbc63f61912c848927eff70d5fe1d75512
Ivan Marchesini <ivan.marchesini@desy.de>
2015-03-27 12:50:04 +0100

    Subjet b-tagging SF measurement with FtCM

---
+++ FtCMTTbar/include/FtCMTTbarHists.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "UHH2/core/include/Hists.h"
+
+namespace uhh2examples {
+
+/**  \brief Example class for booking and filling histograms
+ * 
+ * NOTE: This class uses the 'hist' method to retrieve histograms.
+ * This requires a string lookup and is therefore slow if you have
+ * many histograms. Therefore, it is recommended to use histogram
+ * pointers as member data instead, like in 'common/include/ElectronHists.h'.
+ */
+class FtCMTTbarHists: public uhh2::Hists {
+public:
+    // use the same constructor arguments as Hists for forwarding:
+    FtCMTTbarHists(uhh2::Context & ctx, const std::string & dirname);
+
+    virtual void fill(const uhh2::Event & ev) override;
+    virtual ~FtCMTTbarHists();
+};
+
+}
---
+++ FtCMTTbar/src/FtCMTTbarSelections.cxx
@@ -0,0 +1,25 @@
+#include "UHH2/FtCMTTbar/include/FtCMTTbarSelections.h"
+#include "UHH2/core/include/Event.h"
+
+#include <stdexcept>
+
+using namespace uhh2examples;
+using namespace uhh2;
+
+
+DijetSelection::DijetSelection(float dphi_min_, float third_frac_max_): dphi_min(dphi_min_), third_frac_max(third_frac_max_){}
+    
+bool DijetSelection::passes(const Event & event){
+    assert(event.jets); // if this fails, it probably means jets are not read in
+    if(event.jets->size() < 2) return false;
+    const auto & jet0 = event.jets->at(0);
+    const auto & jet1 = event.jets->at(1);
+    auto dphi = deltaPhi(jet0, jet1);
+    if(dphi < dphi_min) return false;
+    if(event.jets->size() == 2) return true;
+    const auto & jet2 = event.jets->at(2);
+    auto third_jet_frac = jet2.pt() / (0.5 * (jet0.pt() + jet1.pt()));
+    return third_jet_frac < third_frac_max;
+}
+
+
