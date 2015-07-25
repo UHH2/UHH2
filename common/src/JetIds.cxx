1a774e79440d668d9ad00b14286b587eb4253ec6
Ivan Marchesini <ivan.marchesini@desy.de>
2015-07-25 12:20:46 +0200

    New b-tagging CSVv2 operating points

--- common/src/JetIds.cxx
+++ common/src/JetIds.cxx
@@ -6,13 +6,13 @@ using namespace uhh2;
 CSVBTag::CSVBTag(wp working_point) {
     switch(working_point){
         case WP_LOOSE:
-            csv_threshold = 0.423f;
+            csv_threshold = 0.605f;
             break;
         case WP_MEDIUM:
-            csv_threshold = 0.814f;
+            csv_threshold = 0.890f;
             break;
         case WP_TIGHT:
-            csv_threshold = 0.941f;
+            csv_threshold = 0.970f;
             break;
         default:
             throw invalid_argument("invalid working point passed to CSVBTag");
