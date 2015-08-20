d7389b0bd429be93a64fe1eaeccff7bd4274bfb6
Mareike Meyer <mareike.meyer@desy.de>
2015-08-20 11:26:36 +0200

    added HEPTopTagV2, Type2TopTag and Tau2/Tau1

--- common/include/TopJetIds.h
+++ common/include/TopJetIds.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "UHH2/core/include/Event.h"
+#include "UHH2/core/include/Tags.h"
 #include "UHH2/common/include/ObjectIdUtils.h"
 #include "UHH2/common/include/JetIds.h"
 
@@ -34,6 +35,25 @@ public:
   MassType m_typeOfMass;
 };
 
+class Type2TopTag {
+public:
+  
+  enum class MassType {groomed, ungroomed};
+
+  explicit Type2TopTag(double mjetLower=60., double mjetUpper=100., MassType typeOfMass = MassType::ungroomed);
+  explicit Type2TopTag(MassType typeOfMass);
+  
+  bool operator()(const TopJet & topjet, const uhh2::Event & event) const;
+
+ private:
+   double m_mjetLower;
+   double m_mjetUpper;
+   MassType m_typeOfMass;
+};
+
+
+
+
 class HEPTopTag{
 public:
   
@@ -47,6 +67,20 @@ public:
   double m_massWindowUpper;
   double m_cutCondition2;
   double m_cutCondition3;
+};
+
+
+class HEPTopTagV2{
+public:
+  
+  explicit HEPTopTagV2(double minHTTmass=128., double maxHTTmass=171., double maxfRec=0.18);
+  
+  bool operator()(const TopJet & topjet, const uhh2::Event & event) const;
+
+ private:
+   double m_minHTTmass;
+   double m_maxHTTmass;
+   double m_maxfRec;
 };
 
 
@@ -64,6 +98,22 @@ public:
 private:
     double threshold;
 };
+
+/** \brief Cut on tau2/tau1 < threshold
+ * 
+ */
+class Tau21 {
+public:
+    explicit Tau21(double threshold_ = 0.6): threshold(threshold_){}
+    
+    bool operator()(const TopJet & topjet, const uhh2::Event & event) const;
+    
+private:
+    double threshold;
+};
+
+
+
 
 /** \brief Higgs tagger as e.g. used in Rebekka's analysis
  * 
