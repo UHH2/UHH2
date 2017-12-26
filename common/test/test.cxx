c96a0163ab45681ef6d1dade1a3e27c68a71e0f1
Robin Aggleton <robin.aggleton@cern.ch>
2017-12-26 17:41:35 +0100

    Add pragmas to ignore specific warnings from 3rd party libs
    Probably should re-evaluate this at some point, but for now it
    obscures actual compilation messages from UHH code, and
    they are not under our control.

--- common/test/test.cxx
+++ common/test/test.cxx
@@ -1,2 +1,11 @@
 #define BOOST_TEST_MODULE master
+
+// save diagnostic state
+#pragma GCC diagnostic push
+// turn off the specific warning. Can also use "-Wall"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
 #include <boost/test/included/unit_test.hpp>
+
+// turn the warnings back on
+#pragma GCC diagnostic pop
