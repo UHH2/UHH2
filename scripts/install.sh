f7fda0be70265885b1a8ebd45b12d5f630726244
Robin Aggleton <robin.aggleton@cern.ch>
2018-01-12 17:28:15 +0100

    trailing whitespace

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,72 @@
 #!/bin/sh
 
-# might be usefull to call 
+# might be usefull to call
 #
 #   export CMSSW_GIT_REFERENCE=<DIRECTORYWITHENOUGHSPACE>/cmssw.git
 #
 # before running this script
+
+
+# Create default make args for parallel jobs
+if [ -z "$MAKEFLAGS" ]
+then
+	np=$(grep -c ^processor /proc/cpuinfo)
+	let np+=2
+	export MAKEFLAGS="-j$np"
+fi
+
+getToolVersion() {
+    # Get CMSSW tool version using scram
+    # args: <toolname>
+    local toolname="$1"
+    scram tool info "$toolname" | grep -i "Version : " | sed "s/Version : //"
+}
+
+setupFastjet() {
+	FJVER="$1"
+	FJCONTRIBVER="$2"
+	echo "Setting up fastjet $FJVER and fastjet-contrib $FJCONTRIBVER"
+
+	# Create default make args for parallel jobs
+	if [ -z "$MAKEFLAGS" ]
+	then
+		np=$(grep -c ^processor /proc/cpuinfo)
+		let np+=2
+		export MAKEFLAGS="-j$np"
+	fi
+
+	# Setup fastjet & fastjet-contrib
+	# NB use curl not wget as curl available by cvmfs, wget isnt
+	FJINSTALLDIR="$(pwd)/fastjet-install"
+	curl -O http://fastjet.fr/repo/fastjet-${FJVER}.tar.gz
+	tar xzf fastjet-${FJVER}.tar.gz
+	mkdir "${FJINSTALLDIR}"
+	cd fastjet-${FJVER}
+	./configure --prefix="${FJINSTALLDIR}" --enable-allplugins --enable-allcxxplugins CXXFLAGS=-fPIC
+	make $MAKEFLAGS
+	make check
+	make install
+	cd ..
+
+	# Add fastjet-config to PATH
+	export PATH="${FJINSTALLDIR}/bin":$PATH
+
+	curl -O http://fastjet.hepforge.org/contrib/downloads/fjcontrib-${FJCONTRIBVER}.tar.gz
+	tar xzf fjcontrib-${FJCONTRIBVER}.tar.gz
+	cd fjcontrib-${FJCONTRIBVER}
+	# add HOTVR from SVN - do it this way until it becomes a proper contrib
+	svn co http://fastjet.hepforge.org/svn/contrib/contribs/HOTVR/trunk HOTVR/
+	# although we add fastjet-config to path, due to a bug we need to
+	# explicitly state its path to ensure the necessary fragile library gets built
+	./configure --fastjet-config="${FJINSTALLDIR}/bin/fastjet-config" CXXFLAGS=-fPIC
+	make $MAKEFLAGS
+	make check
+	make install
+	# the fragile libs are necessary for CMSSW
+	make fragile-shared
+	make fragile-shared-install
+	cd ..
+}
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 
