3f0d6eb69923fd82d1ba44213e2d0d8b3e89ffe6
Robin Aggleton <robin.aggleton@cern.ch>
2018-01-24 10:06:23 +0100

    Fix conversion from PseudoJet to pat::Jet

--- core/plugins/HOTVRProducer.cc
+++ core/plugins/HOTVRProducer.cc
@@ -273,7 +273,7 @@ HOTVRProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 pat::Jet HOTVRProducer::createPatJet(const PseudoJet & psj)
 {
   pat::Jet newJet;
-  newJet.setP4(math::XYZTLorentzVector(psj.pt(), psj.eta(), psj.phi(), psj.E()));
+  newJet.setP4(math::XYZTLorentzVector(psj.px(), psj.py(), psj.pz(), psj.E()));
   return newJet;
 }
 
