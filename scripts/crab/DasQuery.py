546c073bd5fb4118e3a93903b770b68c93115488
Robin Aggleton <robin.aggleton@cern.ch>
2018-05-03 14:14:58 +0200

    Make DasQuery use cmssw_das_client from CMSSW
    This is due to das_client.py being depreciated in the move to Golang.
    So now there is no more das_client.py, and instead we basically
    call das_client behind the scenes.
    
    Note that we can't pass cert keys or paths anymore,
    but it should still work fine.

--- scripts/crab/DasQuery.py
+++ scripts/crab/DasQuery.py
@@ -1,34 +1,29 @@
 #!/usr/bin/env python
 
 import json
-import os, sys
-import pwd
+import sys
 
-# This is the api for the das such that one can find the datasets
-from das_client import get_data
-from das_client import x509
+from Utilities.General.cmssw_das_client import get_data
 
 def autocomplete_Datasets(data):
     result_array =[]
-    ckey = x509() #os.path.join(os.environ['HOME'], '.globus/userkey.pem')
-    cert = x509() #os.path.join(os.environ['HOME'], '.globus/usercert.pem')
-    cpath = '/etc/grid-security/certificates/' #os.environ['SSL_CERT_DIR'] = '/etc/grid-security/certificates/'
 
     for element in data:
         if '*' in element:
-	  jsondict = get_data('https://cmsweb.cern.ch',"dataset="+element,0,0,0,300,ckey,cert,cpath)
-          #print json.dumps(jsondict, indent=4, sort_keys=True)
-          #print json.dumps(jsondict['data'], indent=4, sort_keys=True)
-          try:
-	    for i in range(len(jsondict['data'])):
-                result_array.append(jsondict['data'][i]['dataset'][0]['name'])
-	  except:
-	    print '='*10
-	    print 'Not found',element
-	    print '='*10
+            jsondict = get_data(host='https://cmsweb.cern.ch',query="dataset="+element,idx=0,limit=0,threshold=300)
+            #print json.dumps(jsondict, indent=4, sort_keys=True)
+            #print json.dumps(jsondict['data'], indent=4, sort_keys=True)
+            try:
+                for i in range(len(jsondict['data'])):
+                    result_array.append(jsondict['data'][i]['dataset'][0]['name'])
+            except:
+                print '='*10
+                print 'Not found',element
+                print '='*10
         else:
             result_array.append(element)
     if len(result_array) == 0: 
-	print "No samples found going to exit"
-	sys.exit(0)
+        print "No samples found going to exit"
+        sys.exit(0)
     return result_array
+
