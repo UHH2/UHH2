f935a0f02c9acfc16050e83b24be6ea81b307aac
Robin Aggleton <robin.aggleton@cern.ch>
2018-05-17 18:38:04 +0200

    2016 MC doesn't have slimmedMETsMuEGClean collection

--- core/python/ntuplewriter.py
+++ core/python/ntuplewriter.py
@@ -7,7 +7,7 @@ useData = True
 if useData:
     met_sources_GL =  cms.vstring("slimmedMETs","slimmedMETsPuppi","slMETsCHS","slimmedMETsMuEGClean","slimmedMETsEGClean","slimmedMETsUncorrected")
 else:
-    met_sources_GL =  cms.vstring("slimmedMETs","slimmedMETsPuppi","slMETsCHS","slimmedMETsMuEGClean")
+    met_sources_GL =  cms.vstring("slimmedMETs","slimmedMETsPuppi","slMETsCHS")
 
 # minimum pt for the large-R jets (applies for all: vanilla CA8/CA15, cmstoptag, heptoptag). Also applied for the corresponding genjets.
 fatjet_ptmin = 150.0
