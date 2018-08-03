8355173cabd37108fc708716b16cb16220a801dc
Robin Aggleton <robin.aggleton@cern.ch>
2018-08-03 10:52:17 +0200

    Add timing to potentially slow commands, remove extra cms-init

--- scripts/install.sh
+++ scripts/install.sh
@@ -82,7 +82,7 @@ setupFastjet() {
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 
 # Get SFrame, do not compile it until we have the right ROOT etc
-git clone https://github.com/UHH2/SFrame.git
+time git clone https://github.com/UHH2/SFrame.git
 
 # Get CMSSW
 export SCRAM_ARCH=slc6_amd64_gcc630
@@ -102,12 +102,12 @@ cd $CMSSW_BASE/src
 time git cms-init -y  # not needed if not addpkg ing
 
 # Necessary for using our FastJet
-git cms-addpkg RecoJets/JetProducers
+time git cms-addpkg RecoJets/JetProducers
 # Necessary for using Fastjet >=3.2.1 to pickup new JetDefinition default arg order
-git cms-addpkg RecoBTag/SecondaryVertex
-git cms-addpkg RecoJets/JetAlgorithms
-git cms-addpkg PhysicsTools/JetMCAlgos
-git cms-addpkg RecoEgamma/ElectronIdentification
+time git cms-addpkg RecoBTag/SecondaryVertex
+time git cms-addpkg RecoJets/JetAlgorithms
+time git cms-addpkg PhysicsTools/JetMCAlgos
+time git cms-addpkg RecoEgamma/ElectronIdentification
 
 # Update FastJet and contribs for HOTVR and UniversalJetCluster
 FJINSTALL=$(fastjet-config --prefix)
@@ -135,6 +135,6 @@ time scram b $MAKEFLAGS
 
 # Get the UHH2 repo & JEC files
 cd $CMSSW_BASE/src
-git clone -b RunII_101_v1 https://github.com/UHH2/UHH2.git
+time git clone -b RunII_101_v1 https://github.com/UHH2/UHH2.git
 cd UHH2
-git clone https://github.com/cms-jet/JECDatabase.git
+time git clone https://github.com/cms-jet/JECDatabase.git
