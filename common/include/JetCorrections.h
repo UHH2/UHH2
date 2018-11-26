a96a354714d31e70f93ebc0237168dd0dcf25e30
Andrea Malara <andrea.malara@cern.ch>
2018-11-26 13:41:39 +0100

    JER SF V3 derived from Pythia and Madgraph samples

--- common/include/JetCorrections.h
+++ common/include/JetCorrections.h
@@ -22,7 +22,7 @@ namespace JERFiles{
   extern const std::vector<std::string> tag##_V##ver##_L1RC_##jetCollection##_MC;\
 \
 
-  
+
 
 #define DEFINE_JERFILES_DATA(tag,ver,jetCollection)\
   extern const std::vector<std::string> tag##_V##ver##_B_L123_noRes_##jetCollection##_DATA;\
@@ -72,7 +72,7 @@ namespace JERFiles{
   DEFINE_JERFILES_MC(Fall17_17Nov2017,6,AK8PFPuppi)
 
   DEFINE_JERFILES_MC(Fall17_17Nov2017,11,AK4PFchs)
-  
+
   DEFINE_JERFILES_MC(Fall17_17Nov2017,23,AK4PFchs)
   DEFINE_JERFILES_MC(Fall17_17Nov2017,24,AK4PFchs)
 
@@ -80,7 +80,7 @@ namespace JERFiles{
   DEFINE_JERFILES_DATA(Fall17_17Nov2017,6,AK8PFPuppi)
 
   DEFINE_JERFILES_DATA(Fall17_17Nov2017,11,AK4PFchs)
-  
+
   DEFINE_JERFILES_DATA(Fall17_17Nov2017,23,AK4PFchs)
   DEFINE_JERFILES_DATA(Fall17_17Nov2017,24,AK4PFchs)
   DEFINE_JERFILES_DATA(Fall17_17Nov2017,25,AK4PFchs)
@@ -88,7 +88,7 @@ namespace JERFiles{
   DEFINE_JERFILES_DATA(Fall17_17Nov2017,27,AK4PFchs)
   DEFINE_JERFILES_DATA(Fall17_17Nov2017,28,AK4PFchs)
   DEFINE_JERFILES_DEcombined(Fall17_17Nov2017,31,AK4PFchs)
-  
+
   DEFINE_JERFILES_DATA(Fall17_09May2018,1,AK4PFchs)
 
 }
@@ -317,6 +317,8 @@ namespace JERSmearing {
   extern const SFtype1 SF_13TeV_Fall17_V2_RunDE;
   extern const SFtype1 SF_13TeV_Fall17_V2_RunBCDEF;
   extern const SFtype1 SF_13TeV_Fall17_V2_RunF_ECAL;
+  extern const SFtype1 SF_13TeV_Fall17_V3_RunBCDEF_Pythia;
+  extern const SFtype1 SF_13TeV_Fall17_V3_RunBCDEF_Madgraph;
 }
 
 
