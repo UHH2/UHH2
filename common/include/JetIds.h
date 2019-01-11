fbc6c914eb74413fa62e2ff2d4a44079283cf120
Andrea Malara <andrea.malara@cern.ch>
2019-01-11 15:14:53 +0100

    update JetID for AK4CHS and create the new ID for AK4PUPPI

--- common/include/JetIds.h
+++ common/include/JetIds.h
@@ -5,19 +5,19 @@
 // see comments in ElectronIds.h for general comments on IDs.
 
 /** \brief The CSV V2 b-tag as jet id
- * 
+ *
  * Cuts on uhh2::Jet::btag_combinedSecondaryVertex, which (in CMSSW) is called "combinedInclusiveSecondaryVertexV2BJetTags"
  * (see core/plugins/NtupleWriterJets.cxx), also known as "CombinedSecondaryVertex v2 with IVF".
- * 
+ *
  * Uses the preliminary thresholds for that tagger recommended here:
  *  https://twiki.cern.ch/twiki/bin/viewauth/CMS/BtagRecommendation53XReReco
- * 
+ *
  * Note that this will certainly need updates once 13TeV recommendations are available.
  */
 class CSVBTag {
 public:
     enum wp {WP_LOOSE, WP_MEDIUM, WP_TIGHT };
-    
+
     explicit CSVBTag(wp working_point);
     explicit CSVBTag(float float_point);
 
@@ -29,7 +29,7 @@ private:
 class DeepCSVBTag {
 public:
     enum wp {WP_LOOSE, WP_MEDIUM, WP_TIGHT };
-    
+
     explicit DeepCSVBTag(wp working_point);
     explicit DeepCSVBTag(float float_point);
 
@@ -45,11 +45,13 @@ private:
  */
 class JetPFID {
  public:
-  enum wp {WP_TIGHT, WP_TIGHT_LEPVETO};
+  enum wp {WP_TIGHT, WP_TIGHT_LEPVETO, WP_TIGHT_PUPPI, WP_TIGHT_LEPVETO_PUPPI};
   explicit JetPFID(wp working_point);
   bool operator()(const Jet&, const uhh2::Event&) const;
  private:
   wp m_working_point;
   bool tightID(const Jet & jet) const;
   bool tightLepVetoID(const Jet & jet) const;
+  bool tightID_PUPPI(const Jet & jet) const;
+  bool tightLepVetoID_PUPPI(const Jet & jet) const;
 };
