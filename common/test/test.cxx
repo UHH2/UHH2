4065979311912e64ef3f2dce3f59b6c1efd177b1
Robin Aggleton <robin.aggleton@cern.ch>
2019-01-24 14:47:05 +0100

    Ignore -Wimplicit-fallthrough from BOOST unit_test header

--- common/test/test.cxx
+++ common/test/test.cxx
@@ -4,6 +4,7 @@
 #pragma GCC diagnostic push
 // turn off the specific warning. Can also use "-Wall"
 #pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
 
 #include <boost/test/included/unit_test.hpp>
 
