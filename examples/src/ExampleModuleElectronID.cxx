91e1258f9645894b96311a12ead5820ef6830547
Robin Aggleton <robin.aggleton@cern.ch>
2019-02-12 15:59:15 +0100

    Add photon ID hists to example electorn module

--- examples/src/ExampleModuleElectronID.cxx
+++ examples/src/ExampleModuleElectronID.cxx
@@ -11,14 +11,15 @@
 using namespace std;
 using namespace uhh2;
 
-/** \brief Example of how to use & check Electron IDs via tags
+/** \brief Example of how to use & check Electron & Photon IDs via tags
  *
- * This is the preferred, POG-approved method to check Electron ID, since it uses
+ * This is the preferred, POG-approved method to check IDs, since it uses
  * stored values made by the official POG recipes.
  * There are manual versions in ElectronIds.cxx, but these may not be up to date.
  *
  * This brief example shows how to use a tag,
  * and produces a histogram to show how many electrons passed each ID.
+ * The same is done for photons
  *
  * For Muons, the approach is similar, but they are called "Selectors"
  */
@@ -32,7 +33,7 @@ public:
     virtual void fill(const uhh2::Event & ev) override;
     virtual ~ExampleElectronIDHists();
 private:
-    TH1I * hElectronIDs;
+    TH1F * hElectronIDs;
     TH2F * hElectronMVAs;
     vector<string> electronIDs;
 };
@@ -41,7 +42,7 @@ ExampleElectronIDHists::ExampleElectronIDHists(Context & ctx, const string & dir
 Hists(ctx, dirname),
 electronIDs(electronIDs_)
 {
-    hElectronIDs = book<TH1I>("ele_ids", ";Electron ID;N", electronIDs.size(), 0, electronIDs.size());
+    hElectronIDs = book<TH1F>("ele_ids", ";Electron ID;N", electronIDs.size(), 0, electronIDs.size());
     // set custom x axis labels
     for (uint i=1;i<=electronIDs.size();i++) {
         hElectronIDs->GetXaxis()->SetBinLabel(i, electronIDs.at(i-1).c_str());
@@ -77,6 +78,47 @@ void ExampleElectronIDHists::fill(const Event & event){
 ExampleElectronIDHists::~ExampleElectronIDHists(){}
 
 
+class ExamplePhotonIDHists: public uhh2::Hists {
+public:
+    ExamplePhotonIDHists(uhh2::Context & ctx, const std::string & dirname, const vector<string> & plectronIDs_);
+
+    virtual void fill(const uhh2::Event & ev) override;
+    virtual ~ExamplePhotonIDHists();
+private:
+    TH1F * hPhotonIDs;
+    vector<string> photonIDs;
+};
+
+ExamplePhotonIDHists::ExamplePhotonIDHists(Context & ctx, const string & dirname, const vector<string> & photonIDs_):
+Hists(ctx, dirname),
+photonIDs(photonIDs_)
+{
+    hPhotonIDs = book<TH1F>("photon_ids", ";Photon ID;N", photonIDs.size(), 0, photonIDs.size());
+    // set custom x axis labels
+    for (uint i=1;i<=photonIDs.size();i++) {
+        hPhotonIDs->GetXaxis()->SetBinLabel(i, photonIDs.at(i-1).c_str());
+    }
+
+}
+
+
+void ExamplePhotonIDHists::fill(const Event & event){
+    // Loop over all IDs for all photons, and store which pass
+    for (auto & phoItr : *event.photons) {
+        for (uint i=0; i < photonIDs.size(); i++) {
+            // Here we want the ID status from the ID string name
+            // Use tagname2tag to convert string to a tag, then use get_tag
+            if (phoItr.get_tag(phoItr.tagname2tag(photonIDs.at(i)))) {
+                hPhotonIDs->Fill(i, event.weight);
+            }
+        }
+    }
+
+}
+
+ExamplePhotonIDHists::~ExamplePhotonIDHists(){}
+
+
 class ExampleModuleElectronID: public AnalysisModule {
 public:
 
@@ -102,26 +144,48 @@ private:
         "mvaEleID_Fall17_iso_V2_wp80",
         "mvaEleID_Fall17_iso_V2_wpLoose"
     };
-    unique_ptr<ExampleElectronIDHists> hists;
+    unique_ptr<ExampleElectronIDHists> eleHists;
+
+    vector<string> photonIDs =  {
+        "cutBasedPhotonID_Spring16_V2p2_loose",
+        "cutBasedPhotonID_Spring16_V2p2_medium",
+        "cutBasedPhotonID_Spring16_V2p2_tight",
+        "mvaPhoID_Spring16_nonTrig_V1_wp90",
+        "mvaPhoID_Spring16_nonTrig_V1_wp80",
+        "cutBasedPhotonID_Fall17_94X_V2_loose",
+        "cutBasedPhotonID_Fall17_94X_V2_medium",
+        "cutBasedPhotonID_Fall17_94X_V2_tight",
+        "mvaPhoID_Fall17_iso_V2_wp90",
+        "mvaPhoID_Fall17_iso_V2_wp80"
+    };
+    unique_ptr<ExamplePhotonIDHists> phoHists;
 };
 
 
 ExampleModuleElectronID::ExampleModuleElectronID(Context & ctx)
 {
     cout << "Hello World from ExampleModuleElectronID!" << endl;
-    hists.reset(new ExampleElectronIDHists(ctx, "electronID", electronIDs));
+    eleHists.reset(new ExampleElectronIDHists(ctx, "electronID", electronIDs));
+    phoHists.reset(new ExamplePhotonIDHists(ctx, "photonID", photonIDs));
 }
 
 
 bool ExampleModuleElectronID::process(Event & event) {
+    cout << " *** event: " << event.event << endl;
 
     for (const auto & eleItr : *event.electrons) {
         // We can use the enum directly, this is the easiest way
         cout << "cutBasedElectronID_Summer16_80X_V1_veto: " << eleItr.get_tag(Electron::cutBasedElectronID_Summer16_80X_V1_veto) << endl;
         cout << "cutBasedElectronID_Fall17_94X_V2_veto: " << eleItr.get_tag(Electron::cutBasedElectronID_Fall17_94X_V2_veto) << endl;
     }
+    for (const auto & phoItr : *event.photons) {
+        // We can use the enum directly, this is the easiest way
+        cout << "cutBasedPhotonID_Spring16_V2p2_loose: " << phoItr.get_tag(Photon::cutBasedPhotonID_Spring16_V2p2_loose) << endl;
+        cout << "mvaPhoID_Fall17_iso_V2_wp80: " << phoItr.get_tag(Photon::mvaPhoID_Fall17_iso_V2_wp80) << endl;
+    }
 
-    hists->fill(event);
+    eleHists->fill(event);
+    phoHists->fill(event);
 
     return true;
 }
