1970e49a3fe17b30da85c62eb1222c2a6439ee8e
Doug Berry <drberry@gmail.com>
2019-04-24 18:17:51 +0200

    QCD 2018 MC

--- core/python/ntuplewriter_mc_2016v3.py
+++ core/python/ntuplewriter_mc_2016v3.py
@@ -13,7 +13,7 @@ process = generate_process(year="2016v3", useData=False)
 
 # Please do not commit changes to source filenames - used for consistency testing
 process.source.fileNames = cms.untracked.vstring([
-    '/store/mc/RunIISummer16MiniAODv3/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/MINIAODSIM/PUMoriond17_94X_mcRun2_asymptotic_v3-v1/00000/02AF6CD5-22C3-E811-B5AA-0CC47AFC3C64.root'
+    '/store/mc/RunIISummer16MiniAODv3/ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M2T4/MINIAODSIM/PUMoriond17_94X_mcRun2_asymptotic_v3-v1/100000/12971E7E-A9BC-E811-A71A-0CC47A1DF800.root'
 ])
 
 # Do this after setting process.source.fileNames, since we want the ability to override it on the commandline
