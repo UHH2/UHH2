b109bf60ddb8d6662cf6b2c80997b1d1b1813f9f
Robin Aggleton <robin.aggleton@cern.ch>
2019-05-08 15:00:38 +0200

    Add examples of RunSwitcher, and combining with YearSwitcher

--- examples/src/ExampleModuleYearRunSwitch.cxx
+++ examples/src/ExampleModuleYearRunSwitch.cxx
@@ -25,7 +25,7 @@ public:
     explicit DummyModule(const std::string & note):
     note_(note)
     {}
-    
+
     virtual bool process(Event & event){
         cout << note_ << endl;
         return true;
@@ -36,55 +36,101 @@ private:
 
 
 /** \brief Example of how to handle setting up modules for different years, run periods
- * 
+ *
  */
 class ExampleModuleYearRunSwitch: public AnalysisModule {
 public:
-    
+
     explicit ExampleModuleYearRunSwitch(Context & ctx);
     virtual bool process(Event & event) override;
 
 private:
-    
-    std::unique_ptr<YearSwitcher> dummyTextAll, jetCleanerAll;
+
+    std::unique_ptr<YearSwitcher> dummyTextYearSwitcher, jetCleanerYearSwitcher, megaYearSwitcher;
+    std::unique_ptr<RunSwitcher> dummyTextRun18, dummyTextRun17;
+
+    // Note that these are raw pointers
+    // FIXME: surely there must be a better way? Do these leak?
+    RunSwitcher *megaRunSwitcher16, *megaRunSwitcher17, *megaRunSwitcher18;
 
 };
 
 
 ExampleModuleYearRunSwitch::ExampleModuleYearRunSwitch(Context & ctx){
     cout << "Hello World from ExampleModuleYearRunSwitch!" << endl;
-    
+
     // Setup and instance of DummyModule module to be run for each year
     // Note that e.g. the 2016 one will be run for both 2016v2 & 2016v3 ntuples
-    dummyTextAll.reset(new YearSwitcher()); 
-    dummyTextAll->setup2016(new DummyModule("I am 2016"));
-    dummyTextAll->setup2017(new DummyModule("I am 2017"));
-    dummyTextAll->setup2018(new DummyModule("I am 2018"));
-    
+    dummyTextYearSwitcher.reset(new YearSwitcher());
+    dummyTextYearSwitcher->setup2016(new DummyModule("I am 2016"));
+    dummyTextYearSwitcher->setup2017(new DummyModule("I am 2017"));
+    dummyTextYearSwitcher->setup2018(new DummyModule("I am 2018"));
+
     // Here we specify a particular module for 2017v2 ntuples. This will be run
     // instead of the one created in setup2017() above.
     // 2017v1 ntuples will still use that one though.
-    dummyTextAll->setup2017v2(new DummyModule("I am 2017v2"));
+    dummyTextYearSwitcher->setup2017v2(new DummyModule("I am 2017v2"));
 
-    
-    // Here is a more realistic example - only for 2017, 
-    // we decide to exclude HF jets and raise the pT cut as well
-    jetCleanerAll.reset(new YearSwitcher()); 
-    jetCleanerAll->setup2016(new JetCleaner(ctx, 30.0, 5.));
-    jetCleanerAll->setup2017(new JetCleaner(ctx, 50.0, 3));
-    jetCleanerAll->setup2018(new JetCleaner(ctx, 30.0, 5));
-    
+    // Here is a more realistic example - we apply some jet cuts,
+    // but only for 2017 we decide to exclude HF jets and raise the pT cut as well
+    jetCleanerYearSwitcher.reset(new YearSwitcher());
+    jetCleanerYearSwitcher->setup2016(new JetCleaner(ctx, 30.0, 5.));
+    jetCleanerYearSwitcher->setup2017(new JetCleaner(ctx, 50.0, 3));
+    jetCleanerYearSwitcher->setup2018(new JetCleaner(ctx, 30.0, 5));
+
+
+    // Here we setup modules for specific run periods in a given year
+    dummyTextRun18.reset(new RunSwitcher("2018"));
+    dummyTextRun18->setupRun("A", new DummyModule("2018 Run A"));
+    dummyTextRun18->setupRun("B", new DummyModule("2018 Run B"));
+    dummyTextRun18->setupRun("C", new DummyModule("2018 Run C"));
+    dummyTextRun18->setupRun("D", new DummyModule("2018 Run D"));
+
+    // much easier way to iterate over all Run periods:
+    dummyTextRun17.reset(new RunSwitcher("2017"));
+    for (const auto & itr : {"B", "C", "D", "E", "F"}) {
+        dummyTextRun17->setupRun(itr, new DummyModule("2017 Run " + std::string(itr)));
+    }
+
+    // Finally, we can also combine these both
+    // Good for e.g. JECs
+    megaRunSwitcher16 = new RunSwitcher("2016");
+    for (const auto & runItr : runPeriods2016) { // runPeriods defined in common/include/Utils.h
+        megaRunSwitcher16->setupRun(runItr, new DummyModule("Super! 2016 + Run " + runItr));
+    }
+
+    megaRunSwitcher17 = new RunSwitcher("2017");
+    for (const auto & runItr : runPeriods2017) {
+        megaRunSwitcher17->setupRun(runItr, new DummyModule("Mega! 2017 + Run " + runItr));
+    }
+
+    megaRunSwitcher18 = new RunSwitcher("2018");
+    for (const auto & runItr : runPeriods2018) {
+        megaRunSwitcher18->setupRun(runItr, new DummyModule("Ausgezeichnet! 2018 + Run " + runItr));
+    }
+
+    megaYearSwitcher.reset(new YearSwitcher());
+    megaYearSwitcher->setup2016(megaRunSwitcher16);
+    megaYearSwitcher->setup2017(megaRunSwitcher17);
+    megaYearSwitcher->setup2018(megaRunSwitcher18);
+
 }
 
 
 bool ExampleModuleYearRunSwitch::process(Event & event) {
-    
+
     cout << "ExampleModuleYearRunSwitch: Starting to process event (runid, eventid) = (" << event.run << ", " << event.event << "); weight = " << event.weight << endl;
-    
-    // the YearSwitcher uses the event.year stored in the ntuple, 
+
+    // the YearSwitcher uses the event.year stored in the ntuple,
     // ignoring whatver is specified in the dataset Version
-    dummyTextAll->process(event);
-    jetCleanerAll->process(event);
+    dummyTextYearSwitcher->process(event);
+    jetCleanerYearSwitcher->process(event);
+
+    // note that for 2016, these are just ignored and all events return true
+    dummyTextRun17->process(event);
+    dummyTextRun18->process(event);
+
+    megaYearSwitcher->process(event);
 
     return true;
 }
