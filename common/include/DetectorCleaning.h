a2c3d47ea2d11b477323c3dbc81977a7c165678e
tom-so <64896708+tom-so@users.noreply.github.com>
2020-09-22 12:40:04 +0200

    Added a DetectorCleaning module.
    Include a module which removes events in case electrons/muons/jets are found in an area specified by an eta and phi interval.
    Includes module HEMCleanerSelection with default values for the HEM15/16 issues.

---
+++ common/include/DetectorCleaning.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "UHH2/core/include/Event.h"
+#include "UHH2/core/include/Selection.h"
+
+#include "UHH2/common/include/Utils.h"
+
+using namespace uhh2;
+using namespace std;
+
+class EtaPhiEventCleaner: public uhh2::Selection {
+public:
+  EtaPhiEventCleaner(uhh2::Context& ctx, int min_run, int max_run, float min_eta, float max_eta, float min_phi, float max_phi, std::string jetCollection = "topjets", bool doJets = true, bool doElectrons = true, bool doMuons = true);
+  virtual bool passes(const uhh2::Event& event) override;
+private:
+  int min_run;
+  int max_run;
+  float min_eta;
+  float max_eta;
+  float min_phi;
+  float max_phi;
+  std::string jetCollection;
+  bool doJets;
+  bool doElectrons;
+  bool doMuons;
+  uhh2::Event::Handle<std::vector<Jet> > h_jets;
+  uhh2::Event::Handle<std::vector<TopJet> > h_topjets;
+};
+
+// Class which inherits from EtaPhiEventCleaner, specific to the HEM15/16 issues.
+// This applies to 2018 only.
+// The values are specified in https://hypernews.cern.ch/HyperNews/CMS/get/JetMET/2000.html
+class HEMCleanerSelection: public EtaPhiEventCleaner{
+public:
+  static const int min_run_HEM = 319077; // end of Run B in 2018
+  static const int max_run_HEM = -1;
+  static constexpr float min_eta_HEM = -2.964;
+  static constexpr float max_eta_HEM = -1.305;
+  static constexpr float min_phi_HEM = -1.6;
+  static constexpr float max_phi_HEM = -0.87;
+
+  HEMCleanerSelection(uhh2::Context& ctx, std::string jetCollection, bool doJets=true, bool doElectrons=true, bool doMuons=true);
+
+private:
+  std::string jetCollection;
+};
