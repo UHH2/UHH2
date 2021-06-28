73e50707709b895a5f7bb85b640014c21714b783
Finn <flabe@physnet.uni-hamburg.de>
2021-06-28 17:16:16 +0200

    re-added missing UserInts

--- core/plugins/PATElectronUserData.cc
+++ core/plugins/PATElectronUserData.cc
@@ -28,12 +28,6 @@ class PATElectronUserData : public edm::EDProducer {
     edm::EDGetToken token;
   };
 
-  edm::ParameterSet      vmaps_bool_;
-  std::vector<vmap_link> vmaps_bool_links_;
-
-  edm::ParameterSet      vmaps_float_;
-  std::vector<vmap_link> vmaps_float_links_;
-
   std::vector<std::string> vmaps_double_;
   std::vector<edm::EDGetToken> vmaps_token_;
 
@@ -51,26 +45,6 @@ PATElectronUserData::PATElectronUserData(const edm::ParameterSet& iConfig):
 
   src_ = consumes< edm::View<pat::Electron> >(iConfig.getParameter<edm::InputTag>("src"));
 
-  if(iConfig.exists("vmaps_bool")) vmaps_bool_ = iConfig.getParameter<edm::ParameterSet>("vmaps_bool");
-  for(unsigned int i=0; i<vmaps_bool_.getParameterNames().size(); ++i){
-
-    vmap_link link;
-    link.vname = vmaps_bool_.getParameterNames().at(i);
-    link.token = consumes< edm::ValueMap<bool> >(vmaps_bool_.getParameter<edm::InputTag>(link.vname));
-
-    vmaps_bool_links_.push_back(link);
-  }
-
-  if(iConfig.exists("vmaps_float")) vmaps_float_ = iConfig.getParameter<edm::ParameterSet>("vmaps_float");
-  for(unsigned int i=0; i<vmaps_float_.getParameterNames().size(); ++i){
-
-    vmap_link link;
-    link.vname = vmaps_float_.getParameterNames().at(i);
-    link.token = consumes< edm::ValueMap<float> >(vmaps_float_.getParameter<edm::InputTag>(link.vname));
-
-    vmaps_float_links_.push_back(link);
-  }
-
   if(iConfig.exists("vmaps_double")) {
     vmaps_double_ = iConfig.getParameter<std::vector<std::string>>("vmaps_double");
     for(unsigned int i=0; i<vmaps_double_.size(); i++){
@@ -85,22 +59,6 @@ void PATElectronUserData::produce(edm::Event& iEvent, const edm::EventSetup& iSe
 
   edm::Handle< edm::View<pat::Electron> > patElecs;
   iEvent.getByToken(src_, patElecs);
-
-  std::vector< edm::Handle< edm::ValueMap<bool> > > vmapBs;
-  for(unsigned int i=0; i<vmaps_bool_links_.size(); ++i){
-
-    edm::Handle< edm::ValueMap<bool> > vmapB;
-    iEvent.getByToken(vmaps_bool_links_.at(i).token, vmapB);
-    vmapBs.push_back(vmapB);
-  }
-
-  std::vector< edm::Handle< edm::ValueMap<float> > > vmapFs;
-  for(unsigned int i=0; i<vmaps_float_links_.size(); ++i){
-
-    edm::Handle< edm::ValueMap<float> > vmapF;
-    iEvent.getByToken(vmaps_float_links_.at(i).token, vmapF);
-    vmapFs.push_back(vmapF);
-  }
 
   std::vector< edm::Handle< edm::ValueMap<double> > > vmapDs;
   for(unsigned int i=0; i<vmaps_token_.size(); ++i){
@@ -118,24 +76,6 @@ void PATElectronUserData::produce(edm::Event& iEvent, const edm::EventSetup& iSe
 
     newElecs->push_back(patElecs->at(i));
     pat::Electron& ele = newElecs->back();
-
-    for(unsigned int j=0; j<vmapBs.size(); ++j){
-
-      if(ele.hasUserInt(vmaps_bool_links_.at(j).vname))
-        throw cms::Exception("InputError") << "@@@ PATElectronUserData::produce -- PAT user-int label already exists: " << vmaps_bool_links_.at(j).vname;
-
-      const bool& val = (*(vmapBs.at(j)))[patElecs->refAt(i)];
-      ele.addUserInt(vmaps_bool_links_.at(j).vname, int(val));
-    }
-
-    for(unsigned int j=0; j<vmapFs.size(); ++j){
-
-      if(ele.hasUserFloat(vmaps_float_links_.at(j).vname))
-        throw cms::Exception("InputError") << "@@@ PATElectronUserData::produce -- PAT user-float label already exists: " << vmaps_float_links_.at(j).vname;
-
-      const float& val = (*(vmapFs.at(j)))[patElecs->refAt(i)];
-      ele.addUserFloat(vmaps_float_links_.at(j).vname, float(val));
-    }
 
     for(unsigned int j=0; j<vmapDs.size(); ++j){
 
