ca3ddaf6c94cde5cf430480e731d6f245617ba4c
apaasch <alexanderpaa94@gmail.com>
2023-05-15 12:18:04 +0200

    Clone Run3_130X_v1 branch

--- scripts/install.sh
+++ scripts/install.sh
@@ -170,7 +170,7 @@ time scram b $MAKEFLAGS
 
 # Get the UHH2 repo & JEC,JER files
 cd $CMSSW_BASE/src
-time git clone -b Run3_124X_v1 https://github.com/UHH2/UHH2.git
+time git clone -b Run3_130X_v1 https://github.com/UHH2/UHH2.git
 cd UHH2
 time git clone https://github.com/cms-jet/JECDatabase.git
 time git clone https://github.com/cms-jet/JRDatabase.git
